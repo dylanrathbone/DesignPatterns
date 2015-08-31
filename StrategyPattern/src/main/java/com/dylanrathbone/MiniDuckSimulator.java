b887d6c2b4ad65843b6170a85975f652b33ed346
Dylan <dylanrathbone@gmail.come>
2015-08-31 08:01:43 +0100

    commit of HeadFirstDesignPatterns strategy pattern example

--- StrategyPattern/src/main/java/com/dylanrathbone/MiniDuckSimulator.java
+++ StrategyPattern/src/main/java/com/dylanrathbone/MiniDuckSimulator.java
@@ -10,18 +10,20 @@ import com.dylanrathbone.model.quack.Sqeak;
 public class MiniDuckSimulator {
 
     public static void main(String [] args){
+
+        //Initialize two ducks of a specific type
         Duck mallard = new MallardDuck();
         Duck modelDuck = new ModelDuck();
 
+        //Make the mallard perform some specific behaviour, notice how the mallard just knows
+        //how it should quack and fly!
         mallard.performQuack();
         mallard.performFly();
-        mallard.swim();
-        mallard.Display();
 
-        //Call the flyBehaviour and quackBehaviour set at modelDuck initialization via constructor
+        //Make the model duck perform some specific behaviour, notice how the mallard just knows
+        //how it should quack and fly!
         modelDuck.performFly();
         modelDuck.performQuack();
-
 
         //Dynamically reset fly and quack behaviour via setter methods
         modelDuck.setFlyBehaviour(new FlyRocketPowered());
@@ -30,7 +32,5 @@ public class MiniDuckSimulator {
         //See how behaviour is changed...
         modelDuck.performFly();
         modelDuck.performQuack();
-        modelDuck.swim();
-        modelDuck.Display();
     }
 }
